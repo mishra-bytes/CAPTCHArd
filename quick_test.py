41c8d3c7a4a24e088159b5c5012d6377aa361a4f
mishra-bytes <adityamishra2634@gmail.com>
2026-05-26 16:13:41 +0530

    feat: add quick_test script for model prediction demonstration

---
+++ quick_test.py
@@ -0,0 +1,14 @@
+﻿import captchard.nsut as nsut
+
+print('Loading model...')
+model = nsut.load_model('model/final_captcha_model.h5')
+
+image_path = 'captchas/captcha_1750587434.jpg'
+print(f'Predicting for: {image_path}')
+
+# Using the new predict alias
+prediction = nsut.predict(image_path, model=model)
+
+print('=' * 20)
+print(f'Prediction Result: {prediction}')
+print('=' * 20)
